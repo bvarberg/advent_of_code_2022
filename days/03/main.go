927abdee2cfdd379eddab2273af18b63a865d878
Byron Varberg <byronvarberg@gmail.com>
2022-12-03 00:30:38 -0600

    Sketch day 03

---
+++ days/03/main.go
@@ -0,0 +1,12 @@
+package main
+
+// Find the item type that appears in both compartments of each rucksack.
+// What is the sum of the priorities of those item types?
+
+func main() {
+	// read file => lines (== rucksacks)
+	// rucksack => compartmentA, compartmentB
+	// intersection(compartmentA, compartmentB) => item type
+	// item type => priority value
+	// sum
+}
