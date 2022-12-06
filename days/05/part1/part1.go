45d9512dd36d015932bf415666d90e0593d82449
Byron Varberg <byronvarberg@gmail.com>
2022-12-05 22:59:19 -0600

    Load initial stack and moves

--- days/05/part1/part1.go
+++ days/05/part1/part1.go
@@ -3,27 +3,62 @@ package part1
 import (
 	"aoc05/stack"
 	"fmt"
+	"readers"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func Run() {
-	s := stack.NewStringStack("A", "B", "C")
-	fmt.Println(s)
+	stacks := loadInitialStacks("./data/simple/initial_stacks.txt")
+	moves := loadMoves("./data/simple/moves.txt")
+	fmt.Println(stacks)
+	fmt.Printf("%+v", moves)
+}
 
-	s.Push("D")
-	fmt.Println(s)
+func loadMoves(name string) (moves []Move) {
+	lines := readers.ReadLines(name)
 
-	s.Pop()
-	s.Pop()
-	fmt.Println(s)
-
-	top, ok := s.Top()
-	if !ok {
-		panic("No top crate")
+	moves = []Move{}
+	for _, line := range lines {
+		moves = append(moves, NewMove(line))
 	}
-	fmt.Println(top)
-	fmt.Println(s)
 
-	s.Pop()
-	s.Pop()
-	fmt.Println(s)
+	return
+}
+
+func loadInitialStacks(name string) (stacks []stack.StringStack) {
+	lines := readers.ReadLines(name)
+
+	stacks = []stack.StringStack{}
+	for _, line := range lines {
+		stacks = append(stacks, stack.NewStringStack(strings.Split(line, ",")...))
+	}
+
+	return
+}
+
+// Move is a structure representing a single rearrangement instruction: `move 1 from 2 to 1`
+type Move struct {
+	// How many?
+	n int
+	// From which source?
+	from int
+	// To which destination?
+	to int
+}
+
+func NewMove(elfReadable string) Move {
+	digitRe := regexp.MustCompile(`\d{1,}`)
+	values := digitRe.FindAllString(elfReadable, -1)
+
+	n, _ := strconv.Atoi(values[0])
+	from, _ := strconv.Atoi(values[1])
+	to, _ := strconv.Atoi(values[2])
+
+	return Move{
+		n,
+		from,
+		to,
+	}
 }
